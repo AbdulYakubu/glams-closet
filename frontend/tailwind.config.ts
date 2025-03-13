1d08e73b4caebccf1d508377842b88855a31b0cc
AaronYeb1 <ayeboah631@st.ug.edu.gh>
2025-03-13 16:45:37 +0000

    Added tailwind

---
+++ frontend/tailwind.config.ts
@@ -0,0 +1,17 @@
+
+import type { Config } from "tailwindcss";
+
+export default {
+  darkMode: ["class"],
+  content: [
+    "./pages/**/*.{ts,tsx,js,jsx}",
+    "./components/**/*.{ts,tsx,js,jsx}",
+    "./app/**/*.{ts,tsx,js,jsx}",
+    "./src/**/*.{ts,tsx,js,jsx}",
+  ],
+  prefix: "",
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+} satisfies Config;
