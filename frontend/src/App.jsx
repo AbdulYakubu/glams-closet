0e90d182e0157212b120547b65289ddc47608ad7
Yakubu Abdul Aziz <yakubuabdulaziz641@gmail.com>
2025-03-14 16:16:31 +0000

    Feat: created a navbar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,23 @@
 import React from 'react'
+import { Routes, Route } from 'react-router-dom'
+import Header from './components/Header'
+import Home from './pages/Home'
+import Contact from './pages/Contact'
+import Collection from './pages/Collection'
+import Testimonial from './pages/Testimonial'
 
 const App = () => {
   return (
-    <div>
-      <p className="text-6xl text-blue-500 font-bold">Hello World</p>
-    </div>
+    <main>
+      <Header />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/collection' element={<Collection />} />
+        <Route path='/contact' element={<Contact />} />
+        <Route path='/testimonial' element={<Testimonial />} />
+        
+      </Routes>
+    </main>
   )
 }
 
---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+
+const Navbar = ({ containerStyles }) => {
+    const navLinks = [
+        { path: '/', title: 'Home' },
+        { path: '/collection', title: 'Collection' },
+        { path: '/testimonial', title: 'Testimonial' },
+        { path: 'mailto:yakubuabdulaziz641@gmail.com', title: 'Contact' },
+    ];
+
+    return (
+        <nav className={`${containerStyles}`}>
+            {navLinks.map((link) => (
+                <NavLink 
+                    key={link.title} 
+                    to={link.path} 
+                    className={({ isActive }) => `${isActive ? "active-link" : ""} px-3 py-2 rounded-full` }
+                >
+                    {link.title}
+                </NavLink>
+            ))}
+        </nav>
+    );
+}
+
+export default Navbar;
